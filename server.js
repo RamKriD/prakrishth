f4d78db81c321d69185dbb19f110126d07a5cff4
Ram Krishna Das <ram.krishna.das@outlook.com>
2021-11-14 19:35:30 +0530

    Adding SSL certificate and HTTPS server

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const fs = require('fs');
 
-const port = process.env.port || 5000;
+const path = require("path");
+const https = require('https');
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+const httpPort = process.env.port || 5000;
+const httpsPort = process.env.port || 5443;
 
 const baseApp = express();
+
+const key = fs.readFileSync('./CA/localhost/localhost.decrypted.key');
+const cert = fs.readFileSync('./CA/localhost/localhost.crt');
+
+
+/**
+ * Session Configuration (New!)
+ */
+
+
+
+/**
+ * Passport Configuration (New!)
+ */
+
+
+/**
+ *  App Configuration
+ */
 
 const utkrishth = require('./server/utkrishth')
 
@@ -13,10 +43,16 @@ baseApp.use(express.static(__dirname + "/client/dist"));
 
 baseApp.use("/utkrishth", utkrishth);
 
-baseApp.get("*", (req, res) => {
+baseApp.get("*", (req, res, next) => {
   res.render(__dirname + "/client/dist/index");
 });
 
-baseApp.listen(port, () => {
-  console.log("base app listening on port 5000");
+const server = https.createServer({ key, cert }, baseApp);
+
+baseApp.listen(httpPort, () => {
+  console.log(`Server is listening on https://localhost:${httpPort}`);
+});
+
+server.listen(httpsPort, () => {
+  console.log(`Server is listening on https://localhost:${httpsPort}`);
 });
