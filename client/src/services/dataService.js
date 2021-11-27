d67de0129cc31d3ea04707f1fe6b1663997e8abe
Ram Krishna Das <ram.krishna.das@outlook.com>
2021-11-27 23:11:09 +0530

    Verification branch

--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -7,15 +7,18 @@ client.defaults.baseURL = baseURL;
 
 client.defaults.timeout = 3000;
 
-client.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  return response.data;
-}, function (error) {
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  // @TODO add server error handling like 404 or 401
-  return Promise.reject(error);
-});
+client.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // @TODO add server error handling like 404 or 401
+    return Promise.reject(error);
+  }
+);
 
 export default client;
